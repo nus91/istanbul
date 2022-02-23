826cffa6a15717e5a95c6005555f325a58101a55
nus91 <97843146+nus91@users.noreply.github.com>
2022-02-23 13:15:14 +0300

    Add files via upload
    this file for pracrice

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:dnaproject/regscreen.dart';
+import 'package:dnaproject/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+import 'accountinfo.dart';
+import 'homefile.dart';
+
+void main() {
+  runApp(const MaterialApp(home: regscreen()),);
+}
+
+class dnaproject extends StatefulWidget {
+  const dnaproject({Key? key}) : super(key: key);
+
+  @override
+  _dnaprojectState createState() => _dnaprojectState();
+}
+
+class _dnaprojectState extends State<dnaproject> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
